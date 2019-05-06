55305f2d1b9eeaac1e369f9023e70df2215c19ff
Kevin Vermaat <kevinvermaat@icloud.com>
2019-05-06 16:15:25 +0200

    new gamesies

--- higher-lower game.py
+++ higher-lower game.py
@@ -6,15 +6,15 @@ import random
 
 while True:
     score = 0
-    number = random.randint(1-10)
+    number = random.randint(1, 10)
     display.show(str(number))
     if button_a.is_pressed():
-        next_number = random.randint(1-10)
+        next_number = random.randint(1, 10)
         if number <= next_number:
             display.show(image.HAPPY)
             score = score + 1
             number = next_number
-            next_number = random.randint(1-10)
+            next_number = random.randint(1, 10)
             sleep(100)
             display.show(str(number))
         elif number >= next_number:
@@ -24,12 +24,12 @@ while True:
             sleep(500)
             break
     if button_b.is_pressed():
-        next_number = random.randint(1-10)
+        next_number = random.randint(1, 10)
         if number >= next_number:
             display.show(image.HAPPY)
             score = score + 1
             number = next_number
-            next_number = random.randint(1-10)
+            next_number = random.randint(1, 10)
             sleep(100)
             display.show(str(number))
         elif number <= next_number:
