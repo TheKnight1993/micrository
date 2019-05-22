c33c200b44b48f9e92bb7c1e17d1b0e7cf61acbb
TheKnight1993 <49590950+TheKnight1993@users.noreply.github.com>
2019-05-22 18:02:39 +0200

    made it work properly
    before it just spammed numbers and you had to hope you hit the right button at the right time, now it shows 1 number

--- higher-lower game.py
+++ higher-lower game.py
@@ -2,38 +2,41 @@ from microbit import *
 import random
 
 
+score = 0
+number = random.randint(1, 10)
+display.show(str(number))
 
 
 while True:
-    score = 0
-    number = random.randint(1, 10)
-    display.show(str(number))
     if button_a.is_pressed():
         next_number = random.randint(1, 10)
         if number <= next_number:
-            display.show(image.HAPPY)
+            display.show(Image.HAPPY)
+            sleep(400)
             score = score + 1
             number = next_number
             next_number = random.randint(1, 10)
             sleep(100)
             display.show(str(number))
         elif number >= next_number:
-            display.show(image.SAD)
+            display.show(Image.SAD)
             sleep(400)
             display.scroll(str(score))
             sleep(500)
             break
+        
     if button_b.is_pressed():
         next_number = random.randint(1, 10)
         if number >= next_number:
-            display.show(image.HAPPY)
+            display.show(Image.HAPPY)
+            sleep(400)
             score = score + 1
             number = next_number
             next_number = random.randint(1, 10)
             sleep(100)
             display.show(str(number))
         elif number <= next_number:
-            display.show(image.SAD)
+            display.show(Image.SAD)
             sleep(400)
             display.scroll(str(score))
             sleep(500)
